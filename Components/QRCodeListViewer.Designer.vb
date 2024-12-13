54abb5511148d7a5afe03725ad2837863d7776ac
ii997 <75733347+ii997@users.noreply.github.com>
2024-12-13 11:14:39 +0800

    fix qrcode report

--- Components/QRCodeListViewer.Designer.vb
+++ Components/QRCodeListViewer.Designer.vb
@@ -22,24 +22,43 @@ Partial Class QRCodeListViewer
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(QRCodeListViewer))
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.DataSet2 = New PRMICI_Billing_System_v2.DataSet2()
+        Me.studentsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.studentsTableAdapter = New PRMICI_Billing_System_v2.DataSet2TableAdapters.studentsTableAdapter()
+        CType(Me.DataSet2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.studentsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ReportViewer1
         '
         Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
         ReportDataSource1.Name = "DataSet1"
-        ReportDataSource1.Value = Nothing
+        ReportDataSource1.Value = Me.studentsBindingSource
         Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "PRMICI_Billing_System_v2.QRCodes.rdlc"
         Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
         Me.ReportViewer1.Name = "ReportViewer1"
         Me.ReportViewer1.ServerReport.BearerToken = Nothing
         Me.ReportViewer1.Size = New System.Drawing.Size(1001, 653)
-        Me.ReportViewer1.TabIndex = 39
-        Me.ReportViewer1.ZoomMode = Microsoft.Reporting.WinForms.ZoomMode.FullPage
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'DataSet2
+        '
+        Me.DataSet2.DataSetName = "DataSet2"
+        Me.DataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'studentsBindingSource
+        '
+        Me.studentsBindingSource.DataMember = "students"
+        Me.studentsBindingSource.DataSource = Me.DataSet2
+        '
+        'studentsTableAdapter
+        '
+        Me.studentsTableAdapter.ClearBeforeFill = True
         '
         'QRCodeListViewer
         '
@@ -51,9 +70,14 @@ Partial Class QRCodeListViewer
         Me.Name = "QRCodeListViewer"
         Me.Text = "QR Codes"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.DataSet2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.studentsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents studentsBindingSource As BindingSource
+    Friend WithEvents DataSet2 As DataSet2
+    Friend WithEvents studentsTableAdapter As DataSet2TableAdapters.studentsTableAdapter
 End Class
